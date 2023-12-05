06f8bb52bfa36d23e8d3dcbcc5a625736b6dcc88
Gabor Roman <roman.gabor@icloud.com>
2023-12-05 16:13:22 +0100

    Initial commit
    Creating Grocery App Shared DTO Package

---
+++ Sources/GroceryAppSharedDTO/LoginResponseDTO.swift
@@ -0,0 +1,22 @@
+//
+//  LoginResponseDTO.swift
+//
+//
+//  Created by Román Gábor on 05/12/2023.
+//
+
+import Foundation
+
+public struct LoginResponseDTO: Codable {
+    public let error: Bool
+    public var reason: String? = nil
+    public var token: String? = nil
+    public var userId: UUID? = nil
+    
+    public init(error: Bool, reason: String? = nil, token: String? = nil, userId: UUID? = nil) {
+        self.error = error
+        self.reason = reason
+        self.token = token
+        self.userId = userId
+    }
+}
---
+++ Sources/GroceryAppSharedDTO/RegisterResponseDTO.swift
@@ -0,0 +1,19 @@
+//
+//  File.swift
+//  
+//
+//  Created by Román Gábor on 05/12/2023.
+//
+
+import Foundation
+
+public struct RegisterResponseDTO: Codable {
+    public let error: Bool
+    public var reason: String? = nil
+    
+    init(error: Bool, reason: String? = nil) {
+        self.error = error
+        self.reason = reason
+    }
+    
+}
