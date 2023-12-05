3cfc5d5eda62b0505f0e12299334149168499769
Gabor Roman <roman.gabor@icloud.com>
2023-12-05 19:02:21 +0100

    Implementing GroceryCategoryRequestDTO and GroceryCategoryResponseDTO - 67

---
+++ Sources/GroceryAppSharedDTO/GroceryCategoryResponseDTO.swift
@@ -0,0 +1,21 @@
+//
+//  GroceryCategoryResponseDTO.swift
+//
+//
+//  Created by Román Gábor on 05/12/2023.
+//
+
+import Foundation
+
+public struct GroceryCategoryResponseDTO: Codable {
+    
+    public let id: UUID
+    public let title: String
+    public let colorCode: String
+    
+    public init(id: UUID, title: String, colorCode: String) {
+        self.id = id
+        self.title = title
+        self.colorCode = colorCode
+    }
+}
