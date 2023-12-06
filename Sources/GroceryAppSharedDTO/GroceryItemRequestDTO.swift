c33a5d21d03fee686ccaef2b5194aa8a71576580
Gabor Roman <roman.gabor@icloud.com>
2023-12-06 14:36:56 +0100

    Added DTOs for GroceryItem

---
+++ Sources/GroceryAppSharedDTO/GroceryItemRequestDTO.swift
@@ -0,0 +1,21 @@
+//
+//  GroceryItemRequestDTO.swift
+//
+//
+//  Created by Román Gábor on 06/12/2023.
+//
+
+import Foundation
+
+public struct GroceryItemRequestDTO: Codable {
+    
+    public let title: String
+    public let price: Double
+    public let quantity: Int
+    
+    public init(title: String, price: Double, quantity: Int) {
+        self.title = title
+        self.price = price
+        self.quantity = quantity
+    }
+}
---
+++ Sources/GroceryAppSharedDTO/GroceryItemResponseDTO.swift
@@ -0,0 +1,23 @@
+//
+//  GroceryItemResponseDTO.swift
+//
+//
+//  Created by Román Gábor on 06/12/2023.
+//
+
+import Foundation
+
+public struct GroceryItemResponseDTO: Codable {
+    
+    public let id: UUID
+    public let title: String
+    public let price: Double
+    public let quantity: Int
+    
+    public init(id: UUID, title: String, price: Double, quantity: Int) {
+        self.id = id
+        self.title = title
+        self.price = price
+        self.quantity = quantity
+    }
+}
